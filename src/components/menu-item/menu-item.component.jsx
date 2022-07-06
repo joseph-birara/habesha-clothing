27892916757d6c48ee669433523194b2f353917d
joseph-birara <josephbirara92@gmail.com>
2022-07-06 22:04:24 +0300

    adding header component

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -15,7 +15,7 @@ const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
       }}
     />
     <div className='content'>
-      <h1 className='title'>{title.toUpperCase()}</h1>
+      <h2 className='title'>{title.toUpperCase()}</h2>
       <span className='subtitle'>SHOP NOW</span>
     </div>
   </div>
