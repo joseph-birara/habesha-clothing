a3e143b269d0e7c9b8a4e42be1fad6e005c53bdd
joseph-birara <josephbirara92@gmail.com>
2022-07-17 00:19:46 +0300

    reselector applied and adding duplicat item removed to increase only quantity

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,8 +1,15 @@
-export const addItemToCart = ({ cartItems, cartItemsToAdd }) => {
-    const existingCartItem = cartItems.find(cartItem => cartItem.id === cartItemsToAdd.id);
-    if (existingCartItem) {
-        return cartItems.map(cartItem => cartItem.id === cartItemsToAdd.id ? { ...cartItem, quantity: quantity + 1 }
-            : {cartItem})
-    }
-    return [...cartItems , {...cartItemsToAdd, quantity:1}]
-}
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToAdd.id
+  );
+
+  if (existingCartItem) {
+    return cartItems.map(cartItem =>
+      cartItem.id === cartItemToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+
+  return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
